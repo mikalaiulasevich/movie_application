4624c42e348ab9bac2b27dd413d69e2864d6e54c
mikolaiulasevich <n.ulasevich@onesoil.ai>
2023-06-22 15:21:33 +0200

    Fix bugs. Compress images. Add persisted state icon.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Project builds and run on latest xcode and android studio without any problems.
+Steps: 
+
+1) yarn
+2) bundle install
+3) cd ios && pod install
+4) Run native builds
--- src/connectivity/client.ts
+++ src/connectivity/client.ts
@@ -1,8 +1,6 @@
 import Axios from 'axios';
 
-// TOKEN FOR TMDB TODO: DELETE and MOVE to ENV
-const TOKEN: string =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTZlOTJmZmY1YzE4MDZiZTFkNDQ1YjNmNzg0M2E0YSIsInN1YiI6IjVhZTIyZjk3OTI1MTQxMjRjNDAxNzc2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IE-vxjowO_8blrEiT7xt2TrhIMuzYQxFv4tFXKA0lSA';
+const TOKEN: string = 'YOUR TMDB TOKEN';
 
 // Impl of http client base on Axios solution
 export const HttpClient = Axios.create({
